df0942e3fe1238228efbd0b1bbc7ffb7a5f540e0
Gbemiga Meduoye <gbemiga@claymoreminds.com>
2018-07-20 10:10:02 +0100

    Added deps

--- src/ViewserverGrid.js
+++ src/ViewserverGrid.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Grid from './GridView';
 import { isEqual, debounce } from 'lodash';
 import DaoDataSource from './DaoDataSource';
